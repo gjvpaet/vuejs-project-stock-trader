3563118c0bc1e1869606c6d88496a3810b1a6c37
Gabriel Joshua Paet <Gabriel Joshua Paet>
2017-04-06 22:23:15 +0800

    Added basic routing and stocks state management scaffolding

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
+import store from './store/store'
+import { routes } from './routes'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+});
 
 new Vue({
   el: '#app',
+  router,
+  store,
   render: h => h(App)
 })
