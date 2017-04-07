60ca9fbd88861f3d9aafbbbdf9d642a9f1885837
Gabriel Joshua Paet <Gabriel Joshua Paet>
2017-04-07 12:03:32 +0800

    Added load and save functionality

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
 import App from './App.vue'
 import store from './store/store'
 import { routes } from './routes'
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-stock-trader-d92f3.firebaseio.com/';
 
 Vue.filter('currency', (value) => {
     return '$' + value.toLocaleString();
