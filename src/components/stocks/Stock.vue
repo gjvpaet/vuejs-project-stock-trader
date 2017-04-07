2c5790140f9a66befa8144e16b6647676d42ed72
Gabriel Joshua Paet <Gabriel Joshua Paet>
2017-04-07 11:02:18 +0800

    Added Buy and Sell stock functionality

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -9,16 +9,25 @@
             </div>
             <div class="panel-body">
                 <div class="pull-left">
-                    <input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
+                    <input type="number" class="form-control" placeholder="Quantity" v-model="quantity"
+                           :class="{danger: insufficientFunds}">
                 </div>
                 <div class="pull-right">
                     <button class="btn btn-success" @click="buyStock"
-                            :disabled="quantity <= 0">Buy</button>
+                            :disabled="quantity <= 0 || insufficientFunds">
+                        {{ insufficientFunds ? 'Insufficient' : 'Buy' }}
+                    </button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
 
 <script>
     export default {
@@ -35,9 +44,16 @@
                     stockPrice: this.stock.price,
                     quantity: this.quantity
                 };
-
-                console.log(order);
+                this.$store.dispatch('buyStock', order);
                 this.quantity = 0;
+            }
+        },
+        computed: {
+            insufficientFunds() {
+                return this.quantity * this.stock.price > this.funds;
+            },
+            funds() {
+                return this.$store.getters.funds;
             }
         }
     }
